27f6334af19bdde81ad52e039074387bddabc5c0
ryanscottjohnson <teacherman1@gmail.com>
2018-11-14 14:04:11 -0800

    webpack and .env

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlPlugin = require('html-webpack-plugin');
+
+const config = {
+  mode: 'development',
+  entry: './client/src/main.js',
+  devServer: {
+    historyApiFallback: true
+  },
+  output: {
+    path: `${__dirname}/client/dist`,
+    filename: 'bundle-[hash].js'
+  },
+  devtool: 'source-map',
+  plugins: [new HtmlPlugin({template: __dirname + '/index.html'})],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: ["babel-loader"],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(scss|css)$/,
+
+        loader: [
+        "style-loader",
+        "css-loader",
+        "sass-loader",
+        "postcss-loader"
+      ]},
+      { 
+        test: /\.(png|jpg)$/, 
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              useRelativePath: true
+            }  
+          }
+        ]
+      },
+    ]
+  },
+  resolve: {
+      extensions: [".js", ".jsx", ".json"]
+  }
+}
+
+module.exports = config;
