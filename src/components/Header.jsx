32038efdfd0c9c29117711c30143b0090cb4f729
ryanscottjohnson <teacherman1@gmail.com>
2019-01-09 14:58:40 -0800

    header.js footer.js and nav.js structure in place

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Nav from "./Nav";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    
+    return (
+      <div className="content">
+        <header className={`header ${this.props.device}`}>
+          <div className="logo">
+            <Link to="/">
+              <div className="logo-container">
+                <div className="image-container">
+                  {/* <img src="../public/logo/logo.svg" alt="TJS LOGO"/> */}
+                </div>
+              </div>
+            </Link>
+            <h2>From Header</h2>
+          </div>
+          <Nav device={this.props.device} handleNavClick={this.props.handleNavClick} {...this.props} />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
